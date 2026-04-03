45eb97fc7a1b6f01b131cfeec7543a3954af8ef7
ariiagafonova <167536860+ariiagaf@users.noreply.github.com>
2026-04-04 00:39:57 +0200

    Revise comments in ViewController.swift
    Updated project notes and author information.

--- Calculator Layout iOS13/ViewController.swift
+++ Calculator Layout iOS13/ViewController.swift
@@ -2,9 +2,10 @@
 //  ViewController.swift
 //  Calculator Layout iOS13
 //
-//  Created by Angela Yu on 01/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
+//📚 STUDENT PROJECT NOTES:
+//  This is a learning project following the App Brewery's iOS course.
+//  Original story structure by Angela Yu. Implementation and extensions by Arina Agafonova
+//  Completed as part of iOS development learning journey - February-Mart 2026
 
 import UIKit
 
